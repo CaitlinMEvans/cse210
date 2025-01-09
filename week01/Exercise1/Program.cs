dd3abc707bfc07479a827f5eec6bf431536ff98c
Caitlin Evans <caitmsevans@gmail.com>
2025-01-08 21:02:23 -0700

    WIP 1 of 5

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Prompt the user to provide their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // PRompt the user to provide their last name 
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display their formatted name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
