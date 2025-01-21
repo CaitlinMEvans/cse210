f0aeb20b2574bf7140456635475581bc803d3f84
Caitlin Evans <caitmsevans@gmail.com>
2025-01-21 15:08:05 -0700

    updated progress tracker to include saving to a file to be appended to the progress tracker.

--- week05/Mindfulness/ProgressTracker.cs
+++ week05/Mindfulness/ProgressTracker.cs
@@ -1,12 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 public class ProgressTracker
 {
     private List<string> _completedActivities = new List<string>();
     private int _totalTime;
+    private const string ProgressFile = "progress.txt"; // File to save progress
+
+    public ProgressTracker()
+    {
+        LoadProgress();
+    }
 
     public void LogActivity(string activityName, int duration)
     {
         _completedActivities.Add(activityName);
         _totalTime += duration;
+        SaveProgress();
     }
 
     public void DisplayProgress()
@@ -32,13 +43,45 @@ public class ProgressTracker
         }
         else if (input == "2")
         {
-            Console.WriteLine("\nThank you for using the Woodland Mindfulness Program - Goodbye!");
+            Console.WriteLine("\nThank you for using the Mindfulness Program. Goodbye!");
             Environment.Exit(0);
         }
         else
         {
             Console.WriteLine("\nInvalid option. Returning to the activities menu...");
             Thread.Sleep(1000);
+        }
+    }
+
+    private void SaveProgress()
+    {
+        using (StreamWriter writer = new StreamWriter(ProgressFile))
+        {
+            writer.WriteLine(_totalTime);
+            foreach (string activity in _completedActivities)
+            {
+                writer.WriteLine(activity);
+            }
+        }
+    }
+
+    private void LoadProgress()
+    {
+        if (File.Exists(ProgressFile))
+        {
+            using (StreamReader reader = new StreamReader(ProgressFile))
+            {
+                if (int.TryParse(reader.ReadLine(), out int time))
+                {
+                    _totalTime = time;
+                }
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    _completedActivities.Add(line);
+                }
+            }
         }
     }
 }
